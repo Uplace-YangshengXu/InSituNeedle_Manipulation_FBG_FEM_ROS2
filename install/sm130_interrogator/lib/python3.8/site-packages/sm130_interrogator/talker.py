c29eea2e90c5014f59ee7a4ef2358a77c2ecd60a
Uplace-YangshengXu <yxu168@jhu.edu>
2023-02-16 15:38:13 -0500

    almost finish pub sub of sm130, build succeed, untested

---
+++ install/sm130_interrogator/lib/python3.8/site-packages/sm130_interrogator/talker.py
@@ -0,0 +1,13 @@
+from sm130_member_function import interrogator_publisher
+
+def main(args = None):
+    rclpy.init(args=args)
+
+    sm130_publisher = interrogator_publisher()
+    rclpy.spin(sm130_publisher)
+    sm130_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
