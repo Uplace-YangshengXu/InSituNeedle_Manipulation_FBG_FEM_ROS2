0bde7e70a6f8449da0538bddde495f78d01e68dc
Uplace-YangshengXu <yxu168@jhu.edu>
2023-02-21 15:49:36 -0500

    finish sm130 publisher and subscriber

--- src/sm130_interrogator/sm130_interrogator/talker.py
+++ src/sm130_interrogator/sm130_interrogator/talker.py
@@ -5,9 +5,13 @@ def main(args = None):
     rclpy.init(args=args)
 
     sm130_publisher = interrogator_publisher()
-    rclpy.spin(sm130_publisher)
-    sm130_publisher.destroy_node()
-    rclpy.shutdown()
+    if sm130_publisher.is_ready == 1:
+        print("sm130 interrogator publisher starts")
+        rclpy.spin(sm130_publisher)
+        sm130_publisher.destroy_node()
+        rclpy.shutdown()
+    else:
+        print("sm130 interrogator publisher fail to start")
 
 
 if __name__ == '__main__':
