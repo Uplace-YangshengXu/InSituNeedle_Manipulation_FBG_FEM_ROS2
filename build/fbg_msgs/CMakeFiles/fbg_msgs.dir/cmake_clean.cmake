2d00778fd3c8e2d8a9157f2440dabc26a8a0b1bb
Your Name <you@example.com>
2023-03-06 22:19:24 -0500

    update the FEM with convergence check, update traj following test

--- build/fbg_msgs/CMakeFiles/fbg_msgs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fbg_msgs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/fbg_msgs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/fbg_msgs/rosidl_generator_py/fbg_msgs/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from fbg_msgs.msg._curvature import Curvature  # noqa: F401
-from fbg_msgs.msg._fbg_reading import FbgReading  # noqa: F401
-from fbg_msgs.msg._needle_shape import NeedleShape  # noqa: F401
