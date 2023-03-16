5e7664aa860d268f5c54eaae3da24b795ca23b89
Uplace-YangshengXu <yxu168@jhu.edu>
2023-03-16 15:51:19 -0400

    update filter_test.m

--- needle_simulation_control/Dataset/filter_test.m
+++ needle_simulation_control/Dataset/filter_test.m
@@ -1,93 +1,94 @@
 % test the effect of Moving average filter
 % created in 05/2/2023 by YangshengXu
 close all;
-num_point = 6000;
+clear;
+AA_selection = [1,5]; % AA1: 1,5
+%AA_selection = [2,6]; % AA2: 2,6
+%AA_selection = [3,7];  % AA3: 3,7
+%AA_selection = [4,8];   % AA4: 4,8
 
-% namefile = 'validation7-9_090.xls'; % data used to validate the calibration matrix
-% num_trial = 7:1:9;
-% curvature = [0,0.25,0.8,1.0,1.25,3.125]; % constant curvature curve
- 
-namefile = 'calibration7-9_090.xls';
-num_trial = 7:1:9;
-curvature = [0 0.5 1.6 2 2.5 3.2];
-
-ilustration = figure;
-set(ilustration,'position',[10,10,2300,700]);
-ax = subplot(1,3,[1 2 3]);
-%ax2 = subplot(1,3,2);
-%ax3 = subplot(1,3,3);
-set(ax,'FontSize',20,'LineWidth',3)
-hold(ax,"on")
-xlabel(ax,'timestamp')
-ylabel(ax,'wavelength')
-
-AA1_ch1_plot = animatedline(ax,'Color','r','LineWidth',3);
-AA2_ch1_plot = animatedline(ax,'Color','g','LineWidth',3);
-AA3_ch1_plot = animatedline(ax,'Color','b','LineWidth',3);
-AA4_ch1_plot = animatedline(ax,'Color','k','LineWidth',3);
-
-%AA1_ch2_plot = animatedline(ax2,'Color','r','LineWidth',3);
-%AA2_ch2_plot = animatedline(ax2,'Color','g','LineWidth',3);
-%AA3_ch2_plot = animatedline(ax2,'Color','b','LineWidth',3);
-%AA4_ch2_plot = animatedline(ax2,'Color','k','LineWidth',3);
 % 
-% AA1_ch3_plot = animatedline(ax3,'Color','r','LineWidth',3);
-% AA2_ch3_plot = animatedline(ax3,'Color','g','LineWidth',3);
-% AA3_ch3_plot = animatedline(ax3,'Color','b','LineWidth',3);
-% AA4_ch3_plot = animatedline(ax3,'Color','k','LineWidth',3);
+% namefile = {'calibration7-9_090.xls','calibration10-12_090.xls'};
+% num_trial = {7:9,10:12};
+% curvature = [0 0.5 1.6 2 2.5 3.2];
 
-legend(ax,'AA1','AA2','AA3','AA4')
-% legend(ax2,'AA1','AA2','AA3','AA4')
-% legend(ax3,'AA1','AA2','AA3','AA4')
-title(ax,'FBG reading')
-% title(ax2,'FBG reading for ch2')
-% title(ax3,'FBG reading for ch3')
 
-% focus on CH1 AA1 1546 - 1547
-%ylim(ax,[1546 1547])
+namefile = {'validation7-9_090.xls','validation10-12_090.xls'}; % data used to validate the calibration matrix
+num_trial = {7:9,10:12};
+curvature = [0,0.25,0.8,1.0,1.25,3.125]; % constant curvature curve
+
+fig1 = figure(1);
+fig2 = figure(2);
+fig3 = figure(3);
+fig4 = figure(4);
+
+
+ax = axes('Parent',fig1);
+ax2 = axes('Parent',fig2);
+ax3 = axes('Parent',fig3);
+ax4 = axes('Parent',fig4);
+
+set([ax,ax2,ax3,ax4],'FontSize',20,'LineWidth',3)
+hold([ax,ax2,ax3,ax4],"on")
+xlabel([ax,ax2,ax3,ax4],'timestamp')
+ylabel([ax,ax2,ax3,ax4],'wavelength')
+
+ax_plot = animatedline(ax,'Color','r','LineWidth',3);
+ax2_plot = animatedline(ax2,'Color','g','LineWidth',3);
+ax3_plot = animatedline(ax3,'Color','b','LineWidth',3);
+ax4_plot = animatedline(ax4,'Color','k','LineWidth',3);
+
+legend(ax,'CH1_0deg')
+legend(ax2,'CH1_90deg')
+legend(ax3,'CH2_0deg')
+legend(ax4,'CH2_90deg')
 
 
 % collect all the data
 trial_0d = []; % only consider the data with 0deg
+trial_90d = [];
+
 for i = 1:size(curvature,2) % for all curves
     curve = num2str(curvature(i));
     % temp compensation
-    for tri = num_trial
-        sheet_name = strcat('trial',num2str(tri),'_',curve,'mm');
-        data = readmatrix(namefile,'Sheet',strcat(sheet_name,'_0deg'));
-        trial_0d = [trial_0d ; data]; % dim: 1*numAA*numCH
-
+    for num_file = 1: size(num_trial,2)
+        disp(namefile{num_file})
+        for tri = num_trial{num_file}
+            sheet_name = strcat('trial',num2str(tri),'_',curve,'mm')
+            
+            data_0 = readmatrix(namefile{num_file},'Sheet',strcat(sheet_name,'_0deg'));
+            trial_0d = [trial_0d ; data_0]; % dim: 1*numAA*numCH
+            data_90 = readmatrix(namefile{num_file},'Sheet',strcat(sheet_name,'_90deg'));
+            trial_90d = [trial_90d ; data_90];
+        end
     end
 end
 
 % get the dimension of data
-[row,col] = size(trial_0d);
+[row_0d,col_0d] = size(trial_0d);
+[row_90d,col_90d] = size(trial_90d);
+% the same
+
 timestamp = 1;
 
-while(timestamp <= row)
-    cal_data = trial_0d(timestamp,:);
-    if timestamp < num_point
-        xlim(ax,[0 num_point-1])
-    else
-        xlim(ax,[timestamp - num_point + 1 timestamp])
-    end
-    
-
-    % in form of ch1AA1 ch1AA2 ch1AA3 ...
-    addpoints(AA1_ch1_plot,timestamp,cal_data(8))
-    %addpoints(AA2_ch1_plot,timestamp,cal_data(2))
-    %addpoints(AA3_ch1_plot,timestamp,cal_data(3))
-    %addpoints(AA4_ch1_plot,timestamp,cal_data(4))
-
-     %addpoints(AA1_ch2_plot,timestamp,cal_data(5))
-     %addpoints(AA2_ch2_plot,timestamp,cal_data(6))
-     %addpoints(AA3_ch2_plot,timestamp,cal_data(7))
-     %addpoints(AA4_ch2_plot,timestamp,cal_data(8))
+while(timestamp <= row_0d)
+    cal_data_0d = trial_0d(timestamp,:);
+    cal_data_90d = trial_90d(timestamp,:);
+%     in form of ch1AA1 ch1AA2 ch1AA3 ...
+    addpoints(ax_plot,timestamp,cal_data_0d(AA_selection(1)))
+    addpoints(ax2_plot,timestamp,cal_data_90d(AA_selection(1)))
+    addpoints(ax3_plot,timestamp,cal_data_0d(AA_selection(2)))
+    addpoints(ax4_plot,timestamp,cal_data_90d(AA_selection(2)))
+% 
+%      addpoints(AA1_ch2_plot,timestamp,cal_data(5))
+%      addpoints(AA2_ch2_plot,timestamp,cal_data(6))
+%      addpoints(AA3_ch2_plot,timestamp,cal_data(7))
+%      addpoints(AA4_ch2_plot,timestamp,cal_data(8))
 %     addpoints(AA1_ch3_plot,timestamp,cal_data(9))
 %     addpoints(AA2_ch3_plot,timestamp,cal_data(10))
 %     addpoints(AA3_ch3_plot,timestamp,cal_data(11))
 %     addpoints(AA4_ch3_plot,timestamp,cal_data(12))
-
+%      
     timestamp = timestamp + 1;
-    %pause(0.01)
+%     pause(0.01)
 end
