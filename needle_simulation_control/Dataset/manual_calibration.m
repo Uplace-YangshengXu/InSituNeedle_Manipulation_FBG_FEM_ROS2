e64e95e2b4b6d94deff0b3140409a0ae9fea361a
Uplace-YangshengXu <yxu168@jhu.edu>
2023-04-14 18:00:16 -0400

    done three more trials of ch3 needle calibration

--- needle_simulation_control/Dataset/manual_calibration.m
+++ needle_simulation_control/Dataset/manual_calibration.m
@@ -8,33 +8,27 @@ clc;
 
 addpath ../
 % collect n data set for each curve
-FBG_data_point = 400;
+FBG_data_point = 200;
 % trial num
-val_repetitions = 1;
-trial_start_num = 9;
+val_repetitions = 3;
+trial_start_num = 1;
 % number of channle and activate area
-channels = 2;
+channels = 3;
 AAs = 4;
+fbg_sensor_num = 10; % 4 + 4 + 2
+
+% curvature of slots on jig
+cal_curve = [0,0.5,1.6,2.0,2.5,3.2]';
+slot_num = length(cal_curve);
+filename = '/calibration_ch3_090.xls';
 
 % % curvature of slots on jig
-% cal_curve = [0,0.5,1.6,2.0,2.5,3.2]';
-% 
-% slot_num = length(cal_curve);
-% filename = '/calibration7-9_090.xls';
-
-% % curvature of slots on jig
-% 
 % cal_curve = [0,0.25,0.8,1.0,1.25,3.125]';
 % slot_num = length(cal_curve);
-% filename = '/validation7-9_090.xls';
-
-cal_curve = 3.2;
-slot_num = length(cal_curve);
-filename = 'trail9calino6deg0.xls';
-cal_rot = 0;
+% filename = '/validation_ch3_090.xls';
 
 % deg
-%cal_rot = [0,90];
+cal_rot = [0,90];
 
 calPath = cd;
 
@@ -62,7 +56,7 @@ for k = trial_start_num:val_repetitions+trial_start_num-1
                     [msg_received,status,statustext] = subscriber.getSubMsg(10);
                     %disp(size(msg_received.signal_reading))
                     % 10 * 1
-                    cal_data = [cal_data;msg_received.signal_reading(1:channels*AAs)'];
+                    cal_data = [cal_data;msg_received.signal_reading(1:fbg_sensor_num)'];
                 end
             end
             
