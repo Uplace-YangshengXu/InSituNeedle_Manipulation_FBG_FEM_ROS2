803097b384de2daa0dba8b98c0b6b3d19ff2a1ac
Uplace-YangshengXu <yxu168@jhu.edu>
2023-04-19 23:16:42 -0400

    finish 3ch 2aa calibration

--- needle_simulation_control/Dataset/customized_cali_mat_generation_3CH.m
+++ needle_simulation_control/Dataset/customized_cali_mat_generation_3CH.m
@@ -1,15 +1,10 @@
-% modified at 19/03/2023
-% this script is used for 2ch or 3ch calibration matrix generation
-% also this script keep the previous version of generate calibration matrix
+% copy and modified from customized_cali_mat_generation.m at 19/03/2023
+% this script is used for 3ch calibration matrix generation
 % created by Yangsheng Xu at Mar. 12 2023
 % this script is used to generate calibration matrix for FBG needle 0006
-% base on data set calibration7-9_090.xls, calibration10-12_090.xls,
-% validation7-9.xls and validation10-12.xls.
 
 % the method used in this script is exactly the same as in
-% Cal_mat_SeperateAA.m, but script use% namefile = {'calibration7-9_090.xls','calibration10-12_090.xls'};
-% num_trial = {7:9,10:12};
-
+% Cal_mat_SeperateAA.m
 
 % this script is highly customized, not suitable for auto calibration
 % matrix generation
@@ -17,38 +12,9 @@
 clear;
 clc;
 
-num_CH = 2; % only 2 channels were used
-num_AA = 4;
-
-
-% addpath ./prev
-% trial_num = {7:9,10:12}; % each data file store 3 trials of measurement for 6 curves
-% calibration_filename = {'calibration7-9_090.xls','calibration10-12_090.xls'};
-% validation_filename = {'validation7-9_090.xls','validation10-12_090.xls'};
-% % data selection
-% cali_ch1_deg0 = {[1,3],
-%             [1,2,3,5,6],
-%             [1,2,3,4,6],
-%             [1,2,6]};
-% cali_ch1_deg90 = {[1:5],
-%             [1:6],
-%             [1:6],
-%             [1,2,4,5,6]};
-%              
-%  vali_ch1_deg0 = {[1,6],
-%             [1,3,4,5,6],
-%             [1,3,4,6],
-%             [1,3,4,6]};
-%  
-%  vali_ch1_deg90 = {[1,2,4,5],
-%             [1:6],
-%             [1:6],
-%             [1,2,5,6]};
-
-
-
-
-trial_num = {1:3};
+num_CH = 3; % 3 channels were used
+num_AA = 4; 
+trial_num = {1:3}; % each data file store 3 trials of measurement for 6 curves
 calibration_filename = {'calibration_ch3_090_processed.xls'};
 validation_filename = {'validation_ch3_090_processed.xls'};
 cali_curve_curvature = [0,0.5,1.6,2.0,2.5,3.2];
@@ -59,28 +25,55 @@ cali_ch1_deg0 = {[1:6],
             [1:6]};
 cali_ch1_deg90 = {[1:6],
             [1:6]};
+        
+%  cali_ch2_deg0 = {[1:6],
+%             [1:6]};
+%  
+%  cali_ch2_deg90 = {[1:6],
+%             [1:6]};
 
-vali_ch1_deg0 = {[1:6],
+% cali_ch3_deg0 = {[1:6],
+%             [1:6]};
+
+% cali_ch3_deg90 = {[1:6],
+%             [1:6]};
+        
+ vali_ch1_deg0 = {[1:6],
             [1:6]};
 
-vali_ch1_deg90 = {[1:6],
-            [1:6]};
  
+ vali_ch1_deg90 = {[1:6],
+            [1:6]};
 
-            
+ 
+%  vali_ch2_deg0 = {[1:6],
+%             [1:6]};
 
+%  
+%  vali_ch2_deg90 = {[1:6],
+%             [1:6]};
+
+%  vali_ch3_deg0 = {[1:6],
+%             [1:6]};
+
+%  vali_ch3_deg90 = {[1;6],
+%             [1:6]};
+
+
+
+          
 %output filename
-cal_name = 'Cal_mat_2CH_2AA_alldata.mat';
+cal_name = 'Cal_mat_3CH_2AA_alldata.mat';
 H = {};
-CS = 'A23'; % condition for channel selection
-
+CS = 'A'; % condition for channel selection
 %% construct meassured data matrix and expected data matrix
 
-for i = 1:2 %use first two AA
+
+for i = 1:2 % only first two AA are used
     real_mat = [];
     measure_mat = [];
     channel_select = [];
-    
+
     if CS == 'A'
         channel_select = [i,i+num_AA,i+num_AA+num_AA]; % for A
     end
@@ -97,8 +90,7 @@ for i = 1:2 %use first two AA
         channel_select = [i+num_AA, i+num_AA+num_AA]; % for A23
     end
 
-
-    % first cal (ch1 and ch2),(deg 0 and 90)
+    % first cal,(deg 0 and 90)
     
     for curve_digi_index = cali_ch1_deg0{i}
         % for 0 deg
@@ -114,7 +106,9 @@ for i = 1:2 %use first two AA
                 
                 data_0 = readmatrix(calibration_filename{num_file},'Sheet',strcat(sheet_name,'_0deg'));
                 
+                % for 3CH only
                 measure_mat = [measure_mat;mean(data_0(:,channel_select) - data_0_unbent(:,channel_select),1)];
+                
                 real_mat = [real_mat; curve_digi,0];
             end
         end
@@ -131,14 +125,15 @@ for i = 1:2 %use first two AA
                 sheet_name_unbent = strcat('trial',num2str(tri),'_0mm');
                 data_90_unbent = readmatrix(calibration_filename{num_file},'Sheet',strcat(sheet_name_unbent,'_90deg'));
                 data_90 = readmatrix(calibration_filename{num_file},'Sheet',strcat(sheet_name,'_90deg'));
-                
+                % for 3CH only
                 measure_mat = [measure_mat;mean(data_90(:,channel_select) - data_90_unbent(:,channel_select),1)];
+                
                 real_mat = [real_mat; 0,curve_digi];
             end
         end
     end
 
-     % second val (ch1 and ch2),(deg 0 and 90)
+     % second val,(deg 0 and 90)
      for curve_digi_index = vali_ch1_deg0{i}
         % for 0 deg
         curve_digi = vali_curve_curvature(curve_digi_index);
@@ -151,6 +146,7 @@ for i = 1:2 %use first two AA
                 sheet_name_unbent = strcat('trial',num2str(tri),'_0mm');
                 data_0_unbent = readmatrix(validation_filename{num_file},'Sheet',strcat(sheet_name_unbent,'_0deg'));
                 data_0 = readmatrix(validation_filename{num_file},'Sheet',strcat(sheet_name,'_0deg'));
+                % for 3CH only
                 measure_mat = [measure_mat;mean(data_0(:,channel_select) - data_0_unbent(:,channel_select),1)];
                 real_mat = [real_mat; curve_digi,0];
             end
@@ -168,8 +164,8 @@ for i = 1:2 %use first two AA
                 sheet_name_unbent = strcat('trial',num2str(tri),'_0mm');
                 data_90_unbent = readmatrix(validation_filename{num_file},'Sheet',strcat(sheet_name_unbent,'_90deg'));
                 data_90 = readmatrix(validation_filename{num_file},'Sheet',strcat(sheet_name,'_90deg'));
-                
-                measure_mat = [measure_mat;mean(data_90(:,channel_select) - data_90_unbent(:,channel_select),1)];
+                % for 3CH only
+                measure_mat = [measure_mat;mean(data_90(:,channel_select) - data_90_unbent(:,channel_select),1)];              
                 real_mat = [real_mat; 0,curve_digi];
             end
         end
@@ -178,8 +174,8 @@ for i = 1:2 %use first two AA
     % get calibration matrix for each AA
     H_sub = inv(transpose(measure_mat)*measure_mat)*transpose(measure_mat)*real_mat;
     predict = measure_mat * H_sub;
-    error =  predict - real_mat;
     H{i} = H_sub;
+    error =  predict - real_mat;
     disp('mean error:');
     disp(mean(abs(error),1));
     disp('max error:');
@@ -190,11 +186,13 @@ for i = 1:2 %use first two AA
     plot(error(:,2));
     legend("xy plane","xz plane");
     grid on;
-    hold off
+    hold off;
 
 
 end
 
-save(cal_name,"H","CS");
+save(cal_name,"H");
+
+
         
     
--- needle_simulation_control/Dataset/filter_test.m
+++ needle_simulation_control/Dataset/filter_test.m
@@ -1,48 +1,63 @@
-% test the effect of Moving average filter
+% this script is used to visulize the data collected during calibration
 % created in 05/2/2023 by YangshengXu
+
 close all;
 clear;
-AA_selection = [1,5]; % AA1: 1,5
-%AA_selection = [2,6]; % AA2: 2,6
+AA_selection = [1,5,9]; % AA1: 1,5
+%AA_selection = [2,6,10]; % AA2: 2,6
 %AA_selection = [3,7];  % AA3: 3,7
 %AA_selection = [4,8];   % AA4: 4,8
 
-% 
+
 % namefile = {'calibration7-9_090.xls','calibration10-12_090.xls'};
 % num_trial = {7:9,10:12};
-% curvature = [0 0.5 1.6 2 2.5 3.2];
+% namefile = {'calibration_ch3_090.xls'}; %collected at 17/03/2023
+namefile = {'calibration_ch3_090_processed.xls'};
+num_trial = {1:3};
+curvature = [0 0.5 1.6 2 2.5 3.2];
 
 
-namefile = {'validation7-9_090.xls','validation10-12_090.xls'}; % data used to validate the calibration matrix
-num_trial = {7:9,10:12};
-curvature = [0,0.25,0.8,1.0,1.25,3.125]; % constant curvature curve
+% %namefile = {'validation7-9_090.xls','validation10-12_090.xls'}; % data used to validate the calibration matrix
+% %namefile = {'validation_ch3_090.xls'}; % data get from ch3 needle, collected at 17/03/2023
+% namefile = {'validation_ch3_090_processed.xls'};
+% num_trial = {1:3};
+% curvature = [0,0.25,0.8,1.0,1.25,3.125]; % constant curvature curve
 
 fig1 = figure(1);
 fig2 = figure(2);
 fig3 = figure(3);
 fig4 = figure(4);
 
+% add for ch3
+fig5 = figure(5);
+fig6 = figure(6);
+
 
 ax = axes('Parent',fig1);
 ax2 = axes('Parent',fig2);
 ax3 = axes('Parent',fig3);
 ax4 = axes('Parent',fig4);
+ax5 = axes('Parent',fig5);
+ax6 = axes('Parent',fig6);
 
-set([ax,ax2,ax3,ax4],'FontSize',20,'LineWidth',3)
-hold([ax,ax2,ax3,ax4],"on")
-xlabel([ax,ax2,ax3,ax4],'timestamp')
-ylabel([ax,ax2,ax3,ax4],'wavelength')
+set([ax,ax2,ax3,ax4,ax5,ax6],'FontSize',20,'LineWidth',3)
+hold([ax,ax2,ax3,ax4,ax5,ax6],"on")
+xlabel([ax,ax2,ax3,ax4,ax5,ax6],'timestamp')
+ylabel([ax,ax2,ax3,ax4,ax5,ax6],'wavelength')
 
 ax_plot = animatedline(ax,'Color','r','LineWidth',3);
 ax2_plot = animatedline(ax2,'Color','g','LineWidth',3);
 ax3_plot = animatedline(ax3,'Color','b','LineWidth',3);
 ax4_plot = animatedline(ax4,'Color','k','LineWidth',3);
+ax5_plot = animatedline(ax5,'Color','c','LineWidth',3);
+ax6_plot = animatedline(ax6,'Color','y','LineWidth',3);
 
 legend(ax,'CH1_0deg')
 legend(ax2,'CH1_90deg')
 legend(ax3,'CH2_0deg')
 legend(ax4,'CH2_90deg')
-
+legend(ax5,'CH3_0deg')
+legend(ax6,'CH3_90deg')
 
 % collect all the data
 trial_0d = []; % only consider the data with 0deg
@@ -54,7 +69,7 @@ for i = 1:size(curvature,2) % for all curves
     for num_file = 1: size(num_trial,2)
         disp(namefile{num_file})
         for tri = num_trial{num_file}
-            sheet_name = strcat('trial',num2str(tri),'_',curve,'mm')
+            sheet_name = strcat('trial',num2str(tri),'_',curve,'mm');
             
             data_0 = readmatrix(namefile{num_file},'Sheet',strcat(sheet_name,'_0deg'));
             trial_0d = [trial_0d ; data_0]; % dim: 1*numAA*numCH
@@ -79,16 +94,8 @@ while(timestamp <= row_0d)
     addpoints(ax2_plot,timestamp,cal_data_90d(AA_selection(1)))
     addpoints(ax3_plot,timestamp,cal_data_0d(AA_selection(2)))
     addpoints(ax4_plot,timestamp,cal_data_90d(AA_selection(2)))
-% 
-%      addpoints(AA1_ch2_plot,timestamp,cal_data(5))
-%      addpoints(AA2_ch2_plot,timestamp,cal_data(6))
-%      addpoints(AA3_ch2_plot,timestamp,cal_data(7))
-%      addpoints(AA4_ch2_plot,timestamp,cal_data(8))
-%     addpoints(AA1_ch3_plot,timestamp,cal_data(9))
-%     addpoints(AA2_ch3_plot,timestamp,cal_data(10))
-%     addpoints(AA3_ch3_plot,timestamp,cal_data(11))
-%     addpoints(AA4_ch3_plot,timestamp,cal_data(12))
+    addpoints(ax5_plot,timestamp,cal_data_0d(AA_selection(3)))
+    addpoints(ax6_plot,timestamp,cal_data_90d(AA_selection(3)))
 %      
     timestamp = timestamp + 1;
-%     pause(0.01)
 end
