900c4b14e04d28a5f9e0556b251e83366d6d33a2
Yanzhou <ywang521@jh.edu>
2023-06-06 17:18:22 -0400

    temp_comp_exp

---
+++ needle_simulation_control/Data_Preprocess/temperature_compensation/temp_compensation_exp.m
@@ -0,0 +1,57 @@
+% this script is to get data for temp compensation
+% 
+clear;
+clc;
+
+addpath ../
+% collect n data set for each curve
+FBG_data_point = 1;
+% trial num
+val_repetitions = 11;
+seq_cur = [0 0.25 0.8 1.0 1.25 3.125 0.5 1.6 2.0 2.5 3.2];
+trial_start_num = 1;
+% number of channle and activate area
+channels = 3;
+AAs = 4;
+fbg_sensor_num = 10; % 4 + 4 + 2
+
+filename = '/temp_exp_formal_T_100.xls';
+
+calPath = cd;
+
+if exist("subscriber",'var')
+        delete(subscriber)
+    end
+% create a matlab subscriber to get curvature reading
+subscriber = MatlabRosPubSub('sub','matlab_fbg_subscriber','/interrogator','fbg_msgs/FbgReading');
+
+clc;
+cal_data = [];
+% read base line
+% for h = 1:FBG_data_point
+%     if exist('subscriber','var')
+%         [msg_received,status,statustext] = subscriber.getSubMsg(10);
+%         % 10 * 1
+%         cal_data = [cal_data;msg_received.signal_reading(1:fbg_sensor_num)'];
+%     end
+% end
+  
+for k = trial_start_num:val_repetitions+trial_start_num-1
+    disp('Press Enter when ready to collect data');
+    pause;
+    
+    disp('reading, hold on...')
+    for h = 1:FBG_data_point
+        if exist('subscriber','var')
+            [msg_received,status,statustext] = subscriber.getSubMsg(10);
+            % 10 * 1
+            cal_data = [cal_data;msg_received.signal_reading(1:fbg_sensor_num)'];
+        end
+    end
+
+    absfileName = strcat(calPath,filename);
+    sheetName = 'Temp_vari_data';
+    writematrix(cal_data,absfileName,'sheet',sheetName);
+
+end
+
